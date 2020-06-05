c696235c281643a52ffcae4f246e0b907730d9a9
Loh Jing Zhi <jingzhi.loh@skymind.my>
2020-06-05 13:53:10 +0800

    add exercise -half done for stock prediction

--- src/main/java/exercise/stockprediction/Exercise.java
+++ src/main/java/exercise/stockprediction/Exercise.java
@@ -1,4 +1,77 @@
 package exercise.stockprediction;
 
+import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.records.reader.impl.collection.CollectionRecordReader;
+import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.split.FileSplit;
+import org.datavec.api.transform.TransformProcess;
+import org.datavec.api.transform.condition.Condition;
+import org.datavec.api.transform.condition.column.ColumnCondition;
+import org.datavec.api.transform.condition.column.NaNColumnCondition;
+import org.datavec.api.transform.filter.ConditionFilter;
+import org.datavec.api.transform.filter.Filter;
+import org.datavec.api.transform.schema.Schema;
+import org.datavec.api.transform.transform.time.DeriveColumnsFromTimeTransform;
+import org.datavec.api.util.ndarray.RecordConverter;
+import org.datavec.api.writable.IntWritable;
+import org.datavec.api.writable.Writable;
+import org.datavec.local.transforms.LocalTransformExecutor;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.joda.time.DateTimeFieldType;
+import org.joda.time.DateTimeZone;
+import org.nd4j.common.io.ClassPathResource;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Exercise {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        File data = new ClassPathResource("/datasets/StockData/AAPL.csv").getFile();
+
+        Schema schema = new Schema.Builder()
+                .addColumnString("Date") //Date,Open,High,Low,Close,Adj Close,Volume
+                .addColumnDouble("Open")
+                .addColumnDouble("High")
+                .addColumnDouble("Low")
+                .addColumnDouble("Close")
+                .addColumnDouble("Adj Close")
+                .addColumnLong("Volume")
+                .build();
+
+        TransformProcess tp = new TransformProcess.Builder(schema)
+                .stringToTimeTransform("Date", "yyyy-MM-dd", DateTimeZone.UTC)
+                .transform(new DeriveColumnsFromTimeTransform.Builder("Date")
+                        .addIntegerDerivedColumn("year", DateTimeFieldType.year())
+                        .addIntegerDerivedColumn("month", DateTimeFieldType.monthOfYear())
+                        .addIntegerDerivedColumn("day", DateTimeFieldType.dayOfMonth())
+                        .addIntegerDerivedColumn("dayOfWeek", DateTimeFieldType.dayOfWeek())
+                        .build())
+                .removeColumns("Date")
+                .build();
+
+        int numLinesToSkip = 1;
+        char delimiter = ',';
+        RecordReader recordReader = new CSVRecordReader(numLinesToSkip, delimiter);
+        recordReader.initialize(new FileSplit(data));
+
+        List<List<Writable>> originalData = new ArrayList<>();
+        while (recordReader.hasNext()) {
+            originalData.add(recordReader.next());
+        }
+
+        List<List<Writable>> processedData = LocalTransformExecutor.execute(originalData, tp);
+        //Create iterator from processedData
+        RecordReader collectionRecordReader = new CollectionRecordReader(processedData);
+//        DataSetIterator iterator = new RecordReaderDataSetIterator(collectionRecordReader,2,4,2);
+        while(collectionRecordReader.hasNext())
+        {
+            System.out.println(collectionRecordReader.next());
+        }
+
+    }
 }
---
+++ src/main/java/exercise/stockprediction/StockIterator.java
@@ -0,0 +1,74 @@
+package exercise.stockprediction;
+
+import org.datavec.api.records.reader.RecordReader;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.api.MultiDataSet;
+import org.nd4j.linalg.dataset.api.MultiDataSetPreProcessor;
+import org.nd4j.linalg.dataset.api.iterator.MultiDataSetIterator;
+
+import java.util.List;
+
+public class StockIterator implements MultiDataSetIterator {
+
+    private int timeStep;
+    private int batchSize;
+    private int currentBatchIdx;
+    private int labelIdx;
+    private int stride;
+    private RecordReader recordReaderData;
+    private List<INDArray> sequenceData;
+
+    public StockIterator(RecordReader recordReaderData, int timeStep, int batchSize, int labelIndex, int stride) {
+        this.currentBatchIdx = 0 ;
+        this.batchSize = batchSize;
+        this.timeStep = timeStep;
+        this.labelIdx = labelIndex;
+        this.stride = stride;
+        sequenceData = getSequenceData(recordReaderData);
+    }
+
+    private List<INDArray> getSequenceData(RecordReader data)
+    {
+        return null;
+    }
+
+    @Override
+    public MultiDataSet next(int i) {
+        return null;
+    }
+
+    @Override
+    public void setPreProcessor(MultiDataSetPreProcessor multiDataSetPreProcessor) {
+
+    }
+
+    @Override
+    public MultiDataSetPreProcessor getPreProcessor() {
+        return null;
+    }
+
+    @Override
+    public boolean resetSupported() {
+        return false;
+    }
+
+    @Override
+    public boolean asyncSupported() {
+        return false;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public MultiDataSet next() {
+        return null;
+    }
+}
